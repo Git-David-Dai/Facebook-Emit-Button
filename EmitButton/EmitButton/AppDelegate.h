b5b32bd2c25a1ca7792f64bc750ea933d7932522
David <david.dai@ubnt.com>
2017-05-23 13:11:35 +0800

    project init

---
+++ EmitButton/EmitButton/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EmitButton
+//
+//  Created by David.Dai on 2017/5/23.
+//  Copyright © 2017年 David.Dai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ EmitButton/EmitButton/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EmitButton
+//
+//  Created by David.Dai on 2017/5/23.
+//  Copyright © 2017年 David.Dai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
